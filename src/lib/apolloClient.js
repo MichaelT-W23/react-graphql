ca7897ab0b01b4188f6a9aa991b9adb7f3347122
MichaelT-W23 <michael.totaro@icloud.com>
2026-02-23 16:10:18 -0500

    Updated apolloClient.js

--- src/lib/apolloClient.js
+++ src/lib/apolloClient.js
@@ -1,8 +1,5 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
-//const graphqlEndpoint = 'https://api.bookql.com/graphql';
-
-// const graphqlEndpoint = 'https://bookql-api-mcp6.onrender.com/graphql';
 // const graphqlEndpoint = 'http://localhost:4000/graphql';
 const graphqlEndpoint = 'https://www.general-api.com/bookql/graphql';
 
